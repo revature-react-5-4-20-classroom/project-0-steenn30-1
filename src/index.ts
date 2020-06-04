0df8982ca5f1d0790d5f9713613e1a3c7897ad0b
steenn30 <steenn30@gmail.com>
2020-06-04 19:22:01 -0400

    yeah

--- src/index.ts
+++ src/index.ts
@@ -151,7 +151,7 @@ app.post('/login', async (req:Request, res:Response) => {
             if(req.session){
                 req.session.user = user;
             }
-            res.send(user).redirect('http://localhost:3000/home');
+            res.send(user);
         } catch(e){
             
             //res.status(400).send(`Failed to authenticate username and password: ${e}`);
